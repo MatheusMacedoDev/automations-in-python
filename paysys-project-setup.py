dc5a1f8f08dabc07cad8b8328cd0c04acc74f13a
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-04-03 22:16:55 -0300

    Open project in neovim  into PaySys setup

--- paysys-project-setup.py
+++ paysys-project-setup.py
@@ -10,12 +10,40 @@ class PaySysSetup:
         self.open_program_by_search('docker')
         self.open_program_by_search('terminal')
 
+        self.open_neovim_project('/mnt/d/Files/Projects/paysys-backend/')
+        self.open_new_terminal_window()
+
     def open_program_by_search(self, program_name):
         pa.press('win')
         pa.write(program_name)
         pa.press('enter')
 
         time.sleep(.5)
+
+    def write_in_terminal(self, command):
+        clipboard.copy(command)
+        pa.hotkey('ctrl', 'v')
+        pa.press('enter')
+
+        time.sleep(.5)
+
+    def open_neovim_project(self, project_path):
+        self.write_in_terminal(f'cd {project_path}')
+        self.write_in_terminal('nvim ./')
+
+        pa.press('enter')
+        pa.press('enter')
+
+    def open_new_terminal_window(self):
+        pa.hotkey('ctrl', 'shift', '5')
+        time.sleep(.5)
+
+    def create_tmux_session(self, session_name):
+        pa.write(f'tmux new -s {session_name}')
+        pa.press('enter')
+
+        time.sleep(.5)
+
 
     
 setup = PaySysSetup()
