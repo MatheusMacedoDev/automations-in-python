915891d8621ba760601b7847b432f0f189f948f5
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-04-06 14:56:59 -0300

    Creating main file format patterns

--- downloaded-files-organizer.py
+++ downloaded-files-organizer.py
@@ -2,7 +2,8 @@ import os
 import re
 
 # Idea
-# - List all files and organize by file format
+# - List all files
+# - Organize by file format
 # - Move organized files to your respective folder
 
 download_directory_path = 'C:/Users/mathe/Downloads'
@@ -13,13 +14,26 @@ class Setup:
 
         all_files = self.list_all_downloaded_files()
 
-        print(all_files)
+        pdfPattern = self.create_file_format_pattern('pdf')
+        jpegPattern = self.create_file_format_pattern('jpeg')
+        jpgPattern = self.create_file_format_pattern('jpg')
+        pngPattern = self.create_file_format_pattern('png')
+        svgPattern = self.create_file_format_pattern('svg')
+        exePattern = self.create_file_format_pattern('exe')
+        xlsxPattern = self.create_file_format_pattern('xlsx')
+        pptxPattern = self.create_file_format_pattern('pptx')
+        isoPattern = self.create_file_format_pattern('iso')
+        partPattern = self.create_file_format_pattern('part')
+        zipPattern = self.create_file_format_pattern('zip')
+        sevenZipPattern = self.create_file_format_pattern('7z')
 
         image_group = []
         exe_group = []
         pdf_group = []
         spreadsheet_group = []
         os_image_group = []
+        slides_group = []
+        compacted_group = []
     
     def list_all_downloaded_files(self):
         files_list = []
@@ -31,5 +45,8 @@ class Setup:
                     files_list.append(file_name)
 
         return files_list
+    
+    def create_file_format_pattern(format_name):
+        return fr'^[\d\w\s\.\-\+\_\(\)\[\]]+\.{format_name}$'
 
 Setup()
