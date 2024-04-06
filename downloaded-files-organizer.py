3e33a89388199704c0cddb1290225dfa419e0caf
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-04-06 15:09:36 -0300

    Filtering the pdf file group for test

--- downloaded-files-organizer.py
+++ downloaded-files-organizer.py
@@ -9,31 +9,42 @@ import re
 download_directory_path = 'C:/Users/mathe/Downloads'
 
 class Setup:
+
     def __init__(self):
+
         self.is_file_pattern = r'^[\d\w\s\.\-\+\_\(\)\[\]]+\.[0-9a-z]{2,5}$'
 
-        all_files = self.list_all_downloaded_files()
+        self.all_files = self.list_all_downloaded_files()
 
-        pdfPattern = self.create_file_format_pattern('pdf')
-        jpegPattern = self.create_file_format_pattern('jpeg')
-        jpgPattern = self.create_file_format_pattern('jpg')
-        pngPattern = self.create_file_format_pattern('png')
-        svgPattern = self.create_file_format_pattern('svg')
-        exePattern = self.create_file_format_pattern('exe')
-        xlsxPattern = self.create_file_format_pattern('xlsx')
-        pptxPattern = self.create_file_format_pattern('pptx')
-        isoPattern = self.create_file_format_pattern('iso')
-        partPattern = self.create_file_format_pattern('part')
-        zipPattern = self.create_file_format_pattern('zip')
-        sevenZipPattern = self.create_file_format_pattern('7z')
+        self.pdf_pattern = self.create_file_format_pattern('pdf')
+        self.jpeg_pattern = self.create_file_format_pattern('jpeg')
+        self.jpg_pattern = self.create_file_format_pattern('jpg')
+        self.png_pattern = self.create_file_format_pattern('png')
+        self.svg_pattern = self.create_file_format_pattern('svg')
+        self.exe_pattern = self.create_file_format_pattern('exe')
+        self.xlsx_pattern = self.create_file_format_pattern('xlsx')
+        self.pptx_pattern = self.create_file_format_pattern('pptx')
+        self.iso_pattern = self.create_file_format_pattern('iso')
+        self.part_pattern = self.create_file_format_pattern('part')
+        self.zip_pattern = self.create_file_format_pattern('zip')
+        self.seven_zip_pattern = self.create_file_format_pattern('7z')
 
-        image_group = []
-        exe_group = []
-        pdf_group = []
-        spreadsheet_group = []
-        os_image_group = []
-        slides_group = []
-        compacted_group = []
+        self.image_group = []
+        self.exe_group = []
+        self.pdf_group = []
+        self.spreadsheet_group = []
+        self.os_image_group = []
+        self.slides_group = []
+        self.compacted_group = []
+
+        for file in self.all_files:
+            if re.match(self.pdf_pattern, file):
+                self.pdf_group.append(file)
+                self.all_files.remove(file)
+                
+        print(self.pdf_group)
+        print(self.all_files)
+
     
     def list_all_downloaded_files(self):
         files_list = []
@@ -46,7 +57,10 @@ class Setup:
 
         return files_list
     
-    def create_file_format_pattern(format_name):
+
+    def create_file_format_pattern(self, format_name):
         return fr'^[\d\w\s\.\-\+\_\(\)\[\]]+\.{format_name}$'
+
+
 
 Setup()
