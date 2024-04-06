fa83aaa4c821ef3cb293f8b6b2918817c33ad218
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-04-06 20:48:51 -0300

    Segragating pdf group logic into classes

--- downloaded-files-organizer.py
+++ downloaded-files-organizer.py
@@ -8,6 +8,9 @@ import re
 
 download_directory_path = 'C:/Users/mathe/Downloads'
 
+def create_file_format_pattern(format_name):
+    return fr'^[\d\w\s\.\-\+\_\(\)\[\]]+\.{format_name}$'
+
 class Setup:
 
     def __init__(self):
@@ -16,18 +19,17 @@ class Setup:
 
         self.all_files = self.list_all_downloaded_files()
 
-        self.pdf_pattern = self.create_file_format_pattern('pdf')
-        self.jpeg_pattern = self.create_file_format_pattern('jpeg')
-        self.jpg_pattern = self.create_file_format_pattern('jpg')
-        self.png_pattern = self.create_file_format_pattern('png')
-        self.svg_pattern = self.create_file_format_pattern('svg')
-        self.exe_pattern = self.create_file_format_pattern('exe')
-        self.xlsx_pattern = self.create_file_format_pattern('xlsx')
-        self.pptx_pattern = self.create_file_format_pattern('pptx')
-        self.iso_pattern = self.create_file_format_pattern('iso')
-        self.part_pattern = self.create_file_format_pattern('part')
-        self.zip_pattern = self.create_file_format_pattern('zip')
-        self.seven_zip_pattern = self.create_file_format_pattern('7z')
+        self.jpeg_pattern = create_file_format_pattern('jpeg')
+        self.jpg_pattern = create_file_format_pattern('jpg')
+        self.png_pattern = create_file_format_pattern('png')
+        self.svg_pattern = create_file_format_pattern('svg')
+        self.exe_pattern = create_file_format_pattern('exe')
+        self.xlsx_pattern = create_file_format_pattern('xlsx')
+        self.pptx_pattern = create_file_format_pattern('pptx')
+        self.iso_pattern = create_file_format_pattern('iso')
+        self.part_pattern = create_file_format_pattern('part')
+        self.zip_pattern = create_file_format_pattern('zip')
+        self.seven_zip_pattern = create_file_format_pattern('7z')
 
         self.image_group = []
         self.exe_group = []
@@ -37,13 +39,18 @@ class Setup:
         self.slides_group = []
         self.compacted_group = []
 
+        self.file_groups = [
+            PdfGroup()
+        ]
+
         for file in self.all_files:
-            if re.match(self.pdf_pattern, file):
-                self.pdf_group.append(file)
-                self.all_files.remove(file)
+            for file_group in self.file_groups:
+                file_group.filter_file(file)
+
+            self.all_files.remove(file)
                 
-        print(self.pdf_group)
-        print(self.all_files)
+        for file_group in self.file_groups:
+            print(file_group.file_group_list)
 
     
     def list_all_downloaded_files(self):
@@ -53,13 +60,39 @@ class Setup:
             for entry in entries:
                 if re.match(self.is_file_pattern, entry.name):
                     file_name = entry.name
-                    files_list.append(file_name)
+
+                files_list.append(file_name)
 
         return files_list
     
 
-    def create_file_format_pattern(self, format_name):
-        return fr'^[\d\w\s\.\-\+\_\(\)\[\]]+\.{format_name}$'
+
+
+
+class FileGroup:
+
+    def __init__(self, folder_group_name, file_format_patterns_list):
+        self.folder_group_name = folder_group_name
+        self.file_format_patterns_list = file_format_patterns_list
+        self.file_group_list = []
+
+    def filter_file(self, file_name):
+        for file_format_pattern in self.file_format_patterns_list:
+            if re.match(file_format_pattern, file_name):
+                self.file_group_list.append(file_name)
+
+
+
+class PdfGroup(FileGroup):
+    def __init__(self):
+
+        pdf_pattern = create_file_format_pattern('pdf')
+
+        patterns_list = [
+            pdf_pattern
+        ]
+
+        super().__init__('PDFs', patterns_list)
 
 
 
